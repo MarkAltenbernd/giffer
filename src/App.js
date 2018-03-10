d6a61e92deeb5eb67fd29e2740c7bdb4fa61b7ac
Mark Altenbernd <Mark@Altenbernd.com>
2018-03-10 08:57:22 -0500

    Passing Giphy project ID from environment into App component

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and
- save to reload.<br />Rev. {rev}
-        </p>
+		<h1>Giphy projecet id: <code>{this.props.giphyID}</code></h1>
+		
+		
       </div>
     );
   }
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const giphyID = "Gb1KY3Lqu93pXZoqaAAEGxWjUd0Tos8A";
+const element = <App giphyID={giphyID} />
+ReactDOM.render(element, document.getElementById('root'));
 registerServiceWorker();
