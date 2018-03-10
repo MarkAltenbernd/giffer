5724d981d908c78fa8391d8505fe3507920e3251
Mark Altenbernd <Mark@Altenbernd.com>
2018-03-10 08:30:38 -0500

    Log statement added to beginning of the App component.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 
 class App extends Component {
   render() {
+	  const rev = "5";
+	  console.log("App.render(" + rev + ")");
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +13,8 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and
+ save to reload.<br />Rev. {rev}
         </p>
       </div>
     );
